18d743f4a179196f5d09cbf4dad533e8ce6ff709
Shaiekh Zayed Bin Hasan <zayed7h2@gmail.com>
2023-05-23 13:49:56 +0600

    C++ solution for 1551A - Polycarp and Coins

---
+++ 1551 - Codeforces Round 734 (Div. 3)/A_Polycarp_and_Coins.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, c1, c2;
+        cin >> n;
+        c1 = n / 3;
+        c2 = n / 3;
+        if (n % 3 == 1)
+        {
+            c1++;
+        }
+        else if (n % 3 == 2)
+        {
+            c2++;
+        }
+        cout << c1 << " " << c2 << endl;
+    }
+}
